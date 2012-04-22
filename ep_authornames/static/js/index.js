5457e82f3e4027852127d7539434e145db8f3c37
Martyn York <etinquis@gmail.com>
2012-04-21 17:16:27 -0700

    added authorname plugin for span title attributes as per https://github.com/Pita/etherpad-lite/issues/297

---
+++ ep_authornames/static/js/index.js
@@ -0,0 +1,40 @@
+exports.acePostWriteDomLineHTML = function(hook_name, args, cb){
+	var className2Author = function (className) {
+	  if (className.substring(0, 7) == "author-") {
+		return className.substring(7).replace(/[a-y0-9]+|-|z.+?z/g, function(cc) {
+		  if (cc == '-') { return '.'; }
+		  else if (cc.charAt(0) == 'z') {
+			return String.fromCharCode(Number(cc.slice(1, -1)));
+		  }
+		  else {
+			return cc;
+		  }
+		});
+	  }
+	  return null;
+	};
+	
+	var className2AuthorName = function(className) {
+		var authorId = className2Author(className);
+		var authorObj = clientVars.collab_client_vars.historicalAuthorData[authorId];
+		if (authorObj) {
+		  return authorObj.name;
+		} else {
+		  return "unknown author: " + authorId;
+		}
+	};
+	
+	$(args.node).children('span').map(
+		function() {
+			console.dir(this); 
+			if(this.classList[0]){
+				var mainClass = this.classList[0];
+				if(mainClass.substring(0,7) === 'author-') {
+					this.title = className2AuthorName(mainClass);
+				}
+			}
+		}
+	);
+	return cb;
+}
+
